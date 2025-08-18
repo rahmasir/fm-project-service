7fe2022c7f7795da454bb035554ac81984df2b37
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-18 16:48:21 +0330

    create: controller (#9)

--- src/main/java/org/rahmasir/fmprojectservice/FmProjectServiceApplication.java
+++ src/main/java/org/rahmasir/fmprojectservice/FmProjectServiceApplication.java
@@ -1,5 +1,7 @@
 package org.rahmasir.fmprojectservice;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +11,12 @@ import org.springframework.cache.annotation.EnableCaching;
  */
 @SpringBootApplication
 @EnableCaching
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
 public class FmProjectServiceApplication {
 
 	public static void main(String[] args) {
